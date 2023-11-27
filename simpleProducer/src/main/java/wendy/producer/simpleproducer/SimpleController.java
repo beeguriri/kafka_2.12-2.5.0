123fcda283612b4fb8095c84ceda5ae4cd751bb9
wendy <wendy@gridwiz.com>
2023-11-27 09:26:28 +0900

    create simple producer project

---
+++ simpleProducer/src/main/java/wendy/producer/simpleproducer/SimpleController.java
@@ -0,0 +1,15 @@
+package wendy.producer.simpleproducer;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j
+public class SimpleController {
+
+    @GetMapping("/")
+    public String test() {
+        return "ok";
+    }
+}
---
+++ simpleProducer/src/test/java/wendy/producer/simpleproducer/SimpleProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package wendy.producer.simpleproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
