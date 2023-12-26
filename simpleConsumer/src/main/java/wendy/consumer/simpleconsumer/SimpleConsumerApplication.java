1c88f7303adff6e5696a8863bee131369d98a034
wendy <wendy@gridwiz.com>
2023-12-26 09:28:05 +0900

    consumer 개발 준비

---
+++ simpleConsumer/src/main/java/wendy/consumer/simpleconsumer/SimpleConsumerApplication.java
@@ -0,0 +1,13 @@
+package wendy.consumer.simpleconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleConsumerApplication.class, args);
+    }
+
+}
---
+++ simpleConsumer/src/test/java/wendy/consumer/simpleconsumer/SimpleConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package wendy.consumer.simpleconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
